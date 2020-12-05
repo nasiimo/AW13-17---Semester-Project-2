443d575c742d45e0e291c02de6d532436ffb603a
Waqar <nasiimo@venora.no>
2020-12-05 23:09:15 +0100

    Having an issue with rendering the home banner and product images.. need assistance here

--- codefiles/js/index.js
+++ codefiles/js/index.js
@@ -2,6 +2,29 @@ import { baseUrl } from "./settings/api.js";
 
 const productsUrl = baseUrl + "products";
 
+const bannerUrl = baseUrl + "home";
+
+console.log(bannerUrl);
+
+(async function () {
+  const bannerContainer = document.querySelector(".home-header");
+
+  try {
+    const bannerResponse = await fetch(bannerUrl);
+    const bannerJson = await bannerResponse.json();
+
+    console.log(bannerJson);
+
+    bannerContainer.innerHTML = "";
+
+    bannerJson.forEach(function (home) {
+      bannerContainer.innerHTML += `${home}`;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
 (async function () {
   const container = document.querySelector(".product-container");
 
@@ -12,12 +35,14 @@ const productsUrl = baseUrl + "products";
     container.innerHTML = "";
 
     json.forEach(function (products) {
-      container.innerHTML += `<div class="product-card" href="product-details.html?id=${products.id}">
+      container.innerHTML += `<div class="product-card">
+                                <a id="redirect" href="product-details.html?id=${products.id}">
                                 <div class="image-container">${products.image}</div>
                                 <div class="info-container">
                                     <p class="product-title">${products.title}</p>
                                     <p class="product-price">$${products.price}</p>
-                                    <button class="btnAdc">Add to cart</button>
+                                </a>
+                                    <button class="btnAtc">Add to cart</button>
                                 </div>
                             </div>`;
     });
