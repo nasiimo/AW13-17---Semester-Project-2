9c66e61efc60dd19930c489543a005458087aec9
Waqar <nasiimo@venora.no>
2020-12-06 20:03:46 +0100

    This commit includes rendering of home banner and product images through two asynchronus api calls

--- codefiles/js/index.js
+++ codefiles/js/index.js
@@ -10,20 +10,20 @@ console.log(bannerUrl);
   const bannerContainer = document.querySelector(".home-header");
 
   try {
-    const bannerResponse = await fetch(bannerUrl);
-    const bannerJson = await bannerResponse.json();
+    const response = await fetch(bannerUrl);
+    const json = await response.json();
 
-    console.log(bannerJson);
+    console.log(json);
 
     bannerContainer.innerHTML = "";
 
-    bannerJson.forEach(function (home) {
-      bannerContainer.innerHTML += `${home}`;
-    });
+    bannerContainer.innerHTML += `<img src="http://localhost:1337/uploads/photo_1544085311_11a028465b03_29d7651dcc.jpeg" height="700px" width="100%"></img>`;
   } catch (error) {
     console.log(error);
   }
 })();
+
+/* ------- */
 
 (async function () {
   const container = document.querySelector(".product-container");
@@ -37,7 +37,7 @@ console.log(bannerUrl);
     json.forEach(function (products) {
       container.innerHTML += `<div class="product-card">
                                 <a id="redirect" href="product-details.html?id=${products.id}">
-                                <div class="image-container">${products.image}</div>
+                                <div class="image-container"><img id ="prod-img" src="http://localhost:1337/uploads/thumbnail_trendest_studio_XZ_3_Em_AI_Wuz0_unsplash_66b7951ba2.jpg"><img></div>
                                 <div class="info-container">
                                     <p class="product-title">${products.title}</p>
                                     <p class="product-price">$${products.price}</p>
