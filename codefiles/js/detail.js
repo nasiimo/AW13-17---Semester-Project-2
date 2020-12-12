b645f0ff3fb7d1a032ca42e114133235a00c9baa
Waqar <nasiimo@venora.no>
2020-12-12 01:32:57 +0100

    working on product details page

--- codefiles/js/detail.js
+++ codefiles/js/detail.js
@@ -12,7 +12,25 @@ const productUrl = baseUrl + "products/" + id;
   const response = await fetch(productUrl);
   const details = response.json();
 
-  const container = document.querySelector(".details-container");
+  const container = document.querySelector(".detail-img");
 
   container.innerHTML = `<img src="http://localhost:1337/uploads/trendest_studio_XZ_3_Em_AI_Wuz0_unsplash_66b7951ba2.jpg" width="100%"><img>`;
 })();
+
+//--
+
+/* (async function () {
+  try {
+    const response = await fetch(productUrl);
+    const json = response.json();
+
+    const container = document.querySelector(".detail-info");
+
+    //Need to iterate over this object with a forEach loop
+    json.forEach(function (products) {
+      container.innerHTML = `<input type="text" placeholder = "${products.id}"/>`;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})(); */
